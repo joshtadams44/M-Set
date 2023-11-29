15ba80f85ff9bd54314029d4f26feb90e3bfbc56
joshtadams44 <150193607+joshtadams44@users.noreply.github.com>
2023-11-28 16:34:02 -0800

    Add files via upload

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -1,13 +1,16 @@
 #include "ComplexPlane.h"
 
 ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
-    : m_pixel_size(pixelWidth, pixelHeight),
-    m_aspectRatio (pixelHeight / pixelWidth),
-    m_plane_size(BASE_WIDTH, BASE_HEIGHT * m_aspectRatio),
-    m_zoomCount(0),
-    m_state(State::CALCULATING),
-    m_vArray(Points, pixelWidth * pixelHeight)
 {
+	m_pixel_size = {pixelWidth, pixelHeight};
+	m_aspectRatio = 1.0*pixelHeight / pixelWidth;
+	m_plane_size = {BASE_WIDTH, BASE_HEIGHT * m_aspectRatio};
+	m_zoomCount = 0;
+	m_state = State::CALCULATING;
+	m_vArray.setPrimitiveType(Points);
+	m_vArray.resize(pixelWidth * pixelHeight);
+
+	//cout << m_pixel_size.x << " " << m_pixel_size.y << endl;
 }
 
 void ComplexPlane::draw(RenderTarget& target, RenderStates states) const
@@ -18,6 +21,7 @@ void ComplexPlane::draw(RenderTarget& target, RenderStates states) const
 void ComplexPlane::updateRender()
 {
 	if (m_state == State::CALCULATING)
+	{
 		for (int j = 0; j < m_pixel_size.x; j++)
 		{
 			for (int i = 0; i < m_pixel_size.y; i++)
@@ -27,10 +31,12 @@ void ComplexPlane::updateRender()
 				Uint8 r, g, b;
 				iterationsToRGB(iterations, r, g, b);
 				m_vArray[j + i * m_pixel_size.x].color = { r, g, b };
+				m_vArray[j + i * m_pixel_size.x].position = {(float)j,(float)i};
 			}
-			m_state = State::DISPLAYING;
+			
 		}
-
+	}
+	m_state = State::DISPLAYING;
 }
 
 void ComplexPlane::zoomIn()
@@ -77,10 +83,11 @@ void ComplexPlane::loadText(Text& text)
 	stringstream gremlin;
 
 	gremlin << "Mandelbrot Set" << endl;
-	gremlin << "Center: " << m_plane_center.x << endl;
+	gremlin << "Center: " << m_plane_center.x << "," << m_plane_center.y << endl;
 	gremlin << "Curser: (" << m_mouseLocation.x << "," << m_mouseLocation.y << ")" << endl;
 	gremlin << "Left_click to Zoom out" << endl;
 	gremlin << "Right-click to Zoom out" << endl;
+	text.setString(gremlin.str());
 
 }
 
@@ -100,17 +107,17 @@ int ComplexPlane::countIterations(Vector2f coord)
 
 void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b)
 {
-	r = (255 * (count) / (MAX_ITER)); 
-	g = (255 * (count) / (MAX_ITER)); 
-	b = (255 * (count) / (MAX_ITER));
+	r = (1.0 * count / MAX_ITER) * 255;  //(255 * (count) / (MAX_ITER)); 
+	g = (1.0 * count / MAX_ITER) * 255;//(255 * (count) / (MAX_ITER)); 
+	b = (1.0 * count / MAX_ITER) * 255;//(255 * (count) / (MAX_ITER));
 }
 
 Vector2f ComplexPlane::mapPixelToCoords(Vector2i mousePixel)
 {
-	float realPart = ((mousePixel.x - 0) / (m_pixel_size.x)) * m_plane_size.x
+	float realPart = ((mousePixel.x - 0) / (1.0*m_pixel_size.x)) * m_plane_size.x
 		+ (m_plane_center.x - m_plane_size.x / 2.0f);
 
-	float imagPart = (mousePixel.y - m_pixel_size.y) / (0 - m_pixel_size.y) * m_plane_size.y
+	float imagPart = (mousePixel.y - m_pixel_size.y) / (0 - 1.0*m_pixel_size.y) * m_plane_size.y
 		+ (m_plane_center.y - m_plane_size.y / 2.0f);
 
 	return Vector2f(realPart, imagPart);
